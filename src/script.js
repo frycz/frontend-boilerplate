4b109c4a95852f6a7c352a95bfbc0a1e31ac77f4
Adam Sawicki <asawicki@najlepszefoto.pl>
2017-02-15 07:48:36 +0100

    Js scripts modified according to eslint suggestions

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 require('./style.css');
 require('file-loader?name=[name].[ext]!./index.html');
-//TODO: https://webpack.github.io/docs/stylesheets.html separate styles
-(function(){
- console.log("General Tutorial");
+// TODO: https://webpack.github.io/docs/stylesheets.html separate styles
+(function init() {
+  console.log('General Tutorial');
 })();
